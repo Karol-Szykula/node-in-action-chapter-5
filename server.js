74cc945375569afb8fa98523fb81ba33d4a7fb51
Karol-Szykula <karol.szykula@wp.pl>
2018-12-20 22:14:29 +0100

    Add saving data on the disk logic

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
-const http = require('http')
+const path = require('path')
 
-let counter = 0
-const server = http.createServer((req, res) => {
-    counter++
-    res.write('I have been opened ' + counter + ' times')
-    res.end()
-})
+const args = process.argv.splice(2)
+const command = args.shift()
+let taskDescription = args.join(' ')
+const file = path.join(process.cwd(), '/tasks')
 
-server.listen(3000)
+switch (command) {
+    case 'list':
+        listTasks(file)
+        break
+    case 'add':
+        addTask(file, taskDescription)
+        break
+    default:
+        console.log('Use: ' + process.argv[0] + 'list|add[opis_zadania]')
+        break
+}
+
+const loadOrInitializeTaskArray = (file, cb) => {
+    fs.exists(file, (exists) => {
+        var tasks = []
+        if (exists) {
+            fs.readFile(file, 'utf8', (err, data) => {
+                if (err) throw err
+                var data = data.toString()
+                var tasks = JSON.parse(data || '[]')
+                cb(tasks)
+            })
+        } else {
+            cb([])
+        }
+    })
+}
+
+const listTasks = (file) => {
+    loadOrInitializeTaskArray(file, (tasks) => {
+        for (var i in tasks) {
+            console.log(tasks[i])
+        }
+    })
+}
+
+const storeTasks = (file, tasks) => {
+    fs.write(file, JSON.stringify(tasks), 'utf8', (err) => {
+        if (err) throw err
+        console.log('Saved')
+    })
+}
+
+const addTask = (file, taskDescription) => {
+    loadOrInitializeTaskArray(file, (tasks) => {
+        tasks.push(taskDescription)
+        storeTasks(file, tasks)
+    })
+}
